b4da48fc07587bf8605b50fecff61e6b33ed0cec
이승구 <leesg107@naver.com>
2024-11-18 21:11:52 +0900

    feat: primevue 적용

--- src/main.js
+++ src/main.js
@@ -6,9 +6,28 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import PrimeVue from 'primevue/config'
+import 'primeflex/primeflex.css'
+import Aura from '@primevue/themes/aura'
+import InputText from 'primevue/inputtext'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.my-app-dark',
+      cssLayer: {
+        name: 'primevue',
+        order: 'tailwind-base, primevue, tailwind-utilities',
+      },
+    },
+  },
+})
+
+app.component('InputText', InputText)
 
 app.mount('#app')
